1101a3e0c05492b329fa727877ee65e5a644051f
shivam-pawar <shivampawar1038@gmail.com>
2023-12-02 21:11:15 +0530

    added support for PWA

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.tsx";
 import store from "./store.ts";
+import { registerSW } from "virtual:pwa-register";
+
 //@ts-ignore
 const root = createRoot(document.getElementById("root"));
-
+// add this to prompt for a refresh
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+});
 root.render(
   <Provider store={store}>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,89 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+const manifestForPlugin = {
+  manifest: {
+    theme_color: "#f69435",
+    background_color: "#f69435",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    short_name: "vite test",
+    description: "testing vite pwa",
+    name: "vite test",
+    icons: [
+      {
+        src: "/icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/icon-256x256.png",
+        sizes: "256x256",
+        type: "image/png",
+      },
+      {
+        src: "/icon-384x384.png",
+        sizes: "384x384",
+        type: "image/png",
+      },
+      {
+        src: "/icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      // add this to cache all the imports
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+      // add this to cache all the
+      // static assets in the public folder
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#f69435",
+        background_color: "#f69435",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "AssayerAssist",
+        description: "A customized assayer report printing software",
+        name: "AssayerAssist",
+        icons: [
+          {
+            src: "/assets/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/assets/256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/assets/384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/assets/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
